310c7e8f338132ce733a4e785c90f74d16b4283b
Abhijit Shingote <ashingote@cinjltmashingote.lan>
2022-10-03 21:58:24 -0400

    aws_eb

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask import render_template
 import requests
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-db=SQLAlchemy()
+# db=SQLAlchemy()
 
 
-def register_extensions(app):
-    db.init_app(app)
+# def register_extensions(app):
+#     db.init_app(app)
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:password@postgres:5432/maindb'
-    register_extensions(app)
+    # app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:password@postgres:5432/maindb'
+    # register_extensions(app)
     return app
 
 app=create_app()
--- install.sh
+++
@@ -1,3 +0,0 @@
-nohup jupyter-lab --no-browser --ip=0.0.0.0 --port=8900 --NotebookApp.password="" --allow-root --NotebookApp.token='' &> /dev/null &
-# python3 app.py
-tail -f /dev/null
