57afaa0c3b514f0d0824a5a6b56636b9f27c4250
Abhijit Shingote <ashingote@cinjltmashingote.lan>
2022-10-03 23:16:44 -0400

    trying rds

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,25 @@
 from flask import Flask
 from flask import render_template
 import requests
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# db=SQLAlchemy()
+db=SQLAlchemy()
 
 
-# def register_extensions(app):
-#     db.init_app(app)
+def register_extensions(app):
+    db.init_app(app)
 
 
 def create_app():
     application = Flask(__name__)
-    # app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:password@postgres:5432/maindb'
-    # register_extensions(app)
+    # application.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:password@postgres:5432/maindb'
+    application.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql+psycopg2://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}:{os.environ['RDS_PORT']}/{os.environ['RDS_DB_NAME']}'
+    register_extensions(application)
     return application
 
 application=create_app()
-# with app.app_context():
-#     db.create_all()
+with application.app_context():
+    db.create_all()
 from app import routes
+
+
