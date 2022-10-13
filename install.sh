3b9509120ae27baa85805d07a887fa63ef32f6d1
Abhijit Shingote <ashingote@cinjltmashingote.lan>
2022-10-13 00:15:14 -0400

    added cron

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # sleep 20s
 nohup jupyter-lab --no-browser --ip=0.0.0.0 --port=8900 --NotebookApp.password="" --allow-root --NotebookApp.token='' &> /dev/null &
 cron
-echo "17 * * * * python3 /app/populate_db_aws_or_container.py  >> /app/load-db.log" | crontab 
+echo "20 * * * * python3 /app/populate_db_aws_or_container.py  >> /app/load-db.log" | crontab 
 python3 config.py
 # tail -f /dev/null
