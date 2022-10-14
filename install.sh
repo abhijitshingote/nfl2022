0351ee8da0471b45cbf824764f9d60178f23c099
Abhijit Shingote <ashingote@cinjltmashingote.lan>
2022-10-13 23:18:14 -0400

    working cron w log

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # sleep 20s
 nohup jupyter-lab --no-browser --ip=0.0.0.0 --port=8900 --NotebookApp.password="" --allow-root --NotebookApp.token='' &> /dev/null &
 cron
-echo "35 0,20 * * python3 /app/populate_db_aws_or_container.py  >> /app/load-db.log" | crontab 
+echo "22 3 * * * python3 -u /app/populate_db_aws_or_container.py >> /app/load-db.log 2>&1 " | crontab 
+# echo "* * * * * echo helloRad >> /app/load-db.log 2>&1 " | crontab 
 python3 config.py
 # tail -f /dev/null
